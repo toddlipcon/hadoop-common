dcc33e471f84bdbf35beced98e636ba3c60e1291
Todd Lipcon <todd@cloudera.com>
2011-10-28 16:26:57 -0700

    HDFS-2511. Add script to generate HDFS protobufs

---
+++ hadoop-hdfs-project/hadoop-hdfs/dev-support/generate-protos.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+PROTOC=${PROTOC:-protoc}
+HDFS_SRC_ROOT=$(dirname $0)/..
+JAVA_GEN_DIR=src/main/java/org/apache/hadoop/hdfs/protocol/proto/
+
+cd $HDFS_SRC_ROOT
+
+echo generating protobuf code...
+$PROTOC -Isrc/proto --java_out=src/main/java src/proto/*
+
+echo inserting licenses...
+for x in $(grep -L "Licensed to the Apache Software Foundation" $JAVA_GEN_DIR/* ) ; do
+  TMP=/tmp/tmp.$$
+  cat > $TMP <<EOF
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+EOF
+  cat $x >> $TMP
+  mv $TMP $x
+done
+
+echo done
